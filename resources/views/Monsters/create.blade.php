4cb6cfe4033bc87ae918fb98fbb8c2851dd1a67f
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-17 17:09:26 +0200

    form create

--- resources/views/Monsters/create.blade.php
+++ resources/views/Monsters/create.blade.php
@@ -15,36 +15,94 @@
                         </ul>
                     </div>
                 @endif
-                <form method="POST" action="{{ route('comics.store') }}">
+                <form method="POST" action="{{ route('monsters.store') }}">
                     @csrf
                     <div class="mb-3">
-                        <label for="title" class="form-label">Titolo</label>
-                        <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+                        <label for="index" class="form-label">Index</label>
+                        <input type="text" class="form-control" id="index" name="index" value="{{ old('index') }}">
                     </div>
+
                     <div class="mb-3">
-                        <label for="description" class="form-label">Descrizione</label>
-                        <textarea class="form-control" name="description" id="description">{{ old('description') }}</textarea>
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" textarea class="form-control" name="name" id="name">{{ old('name') }}</textarea>
                     </div>
+
                     <div class="mb-3">
-                        <label for="thumb" class="form-label">Immagine</label>
-                        <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb') }}">
-                    </div>
-                    <div class="mb-3">
-                        <label for="price'" class="form-label">Prezzo</label>
-                        <input type="number" class="form-control" id="price" name="price" value="{{ old('price') }}">
-                    </div>
-                    <div class="mb-3">
-                        <label for="series" class="form-label">Serie</label>
-                        <input type="text" class="form-control" id="series" name="series" value="{{ old('series') }}">
-                    </div>
-                    <div class="mb-3">
-                        <label for="sale_date" class="form-label">Data vendita</label>
-                        <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date') }}">
-                    </div>
-                    <div class="mb-3">
-                        <label for="type" class="form-label">Tipo</label>
+                        <label for="type" class="form-label">Type</label>
                         <input type="text" class="form-control" id="type" name="type" value="{{ old('type') }}">
                     </div>
+
+                    <div class="mb-3">
+                        <label for="alignment" class="form-label">Alignment</label>
+                        <input type="text" class="form-control" id="alignment" name="alignment" value="{{ old('alignment') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="size" class="form-label">Size</label>
+                        <input type="text" class="form-control" id="size" name="size" value="{{ old('size') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="hit_dice" class="form-label">Hit_dice</label>
+                        <input type="text" class="form-control" id="hit_dice" name="hit_dice" value="{{ old('hit_dice') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="languages" class="form-label">Languages</label>
+                        <input type="text" class="form-control" id="languages" name="languages" value="{{ old('languages') }}">
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="armor_class'" class="form-label">Armor_class</label>
+                        <input type="number" class="form-control" id="armor_class" name="armor_class" value="{{ old('armor_class') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="hit_points'" class="form-label">Hit_points</label>
+                        <input type="number" class="form-control" id="hit_points" name="hit_points" value="{{ old('hit_points') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="strength'" class="form-label">Strength</label>
+                        <input type="number" class="form-control" id="strength" name="strength" value="{{ old('strength') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="dexterity'" class="form-label">Dexterity</label>
+                        <input type="number" class="form-control" id="dexterity" name="dexterity" value="{{ old('dexterity') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="constitution'" class="form-label">Constitution</label>
+                        <input type="number" class="form-control" id="constitution" name="constitution" value="{{ old('constitution') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="intelligence'" class="form-label">Intelligence</label>
+                        <input type="number" class="form-control" id="intelligence" name="intelligence" value="{{ old('intelligence') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="wisdom'" class="form-label">wisdom</label>
+                        <input type="number" class="form-control" id="wisdom" name="wisdom" value="{{ old('wisdom') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="charisma'" class="form-label">charisma</label>
+                        <input type="number" class="form-control" id="charisma" name="charisma" value="{{ old('charisma') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="xp'" class="form-label">Xp</label>
+                        <input type="number" class="form-control" id="xp" name="xp" value="{{ old('xp') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="challenge_rating'" class="form-label">Challenge_rating</label>
+                        <input type="number" class="form-control" id="challenge_rating" name="challenge_rating" value="{{ old('challenge_rating') }}">
+                    </div>
+                   
                     <button type="submit" class="btn btn-primary">Invia</button>
                 </form>
             </div>
