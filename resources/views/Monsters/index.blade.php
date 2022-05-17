4d6c3328a0814556bd3982eb122028975028f07b
MattFofo <matteo.segattini2@gmail.com>
2022-05-17 17:39:12 +0200

    route shouw + edit

--- resources/views/Monsters/index.blade.php
+++ resources/views/Monsters/index.blade.php
@@ -10,13 +10,13 @@
         <div class="row g-4">
             @foreach ($monsters as $monster)
             <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-                {{-- <a class="text-decoration-none text-dark" href="{{ route('monster.show', $monster) }}"> --}}
+                <a class="text-decoration-none text-dark" href="{{ route('monsters.show', $monster) }}">
                     <div class="card h-100 bg-card justify-content-between">
 
                         <h4 class="text-dark p-2">
                             {{ $monster->name }}
                         </h4>
-                        {{-- <a class="btn btn-dark" href="{{ route('monsters.edit', $monster) }}">EDIT</a> --}}
+                        <a class="btn btn-dark" href="{{ route('monsters.edit', $monster) }}">EDIT</a>
 
                     </div>
                 </a>
